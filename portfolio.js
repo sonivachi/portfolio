46ee18cc16c14278ab75ca9f9263ee0bf5f0462f
sonivachi <sonivachi123@gmail.com>
2026-07-04 16:36:29 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries)=>{
+
+  entries.forEach((entry)=>{
+
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+
+  });
+
+});
+
+document.querySelectorAll(".hidden").forEach((el)=>{
+  observer.observe(el);
+});
+
